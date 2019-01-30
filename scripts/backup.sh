91f7521733e012f61a0d8f993561b63b250413c2
unknown <bionicbiplov45@gmail.com>
2019-01-30 17:51:28 +0700

    added backup script

---
+++ scripts/backup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir /var/backups/postfirerecovery
+chown postgres /var/backups/postfirerecovery
+NOW=\$(date +"%Y-%m-%d")
+su -c - postgres "pg_dump -c postfirerecovery > /var/backups/postfirerecovery/postfirerecovery-\$NOW.sql"
+OLD=\$(date --date='7 day ago' +"%Y-%m-%d")
+rm -f /var/backups/postfirerecovery/postfirerecovery-\$OLD.sql
+mkdir /var/backups/postfirerecovery/uploads
