fd46ae136bfa06c485b4cecaab62b06d2dd7d875
unknown <bionicbiplov45@gmail.com>
2019-06-17 11:53:08 +0700

    update new fire area in the select dropdown

--- scripts/list-firenames.py
+++ scripts/list-firenames.py
@@ -4,7 +4,7 @@ import os
 from os.path import isfile, join
 
 os.path.dirname(os.path.dirname(__file__))
-path = os.path.join(os.getcwd() ,'../postfirerecovery/static/data/fire_area_by_name')
+path = os.path.join(os.getcwd() ,'../postfirerecovery/static/data/fire_area_by_name/geojson')
 path = os.path.abspath(path)
 
 files = [f.split('.geo.json')[0] for f in os.listdir(path) if isfile(join(path, f))]
