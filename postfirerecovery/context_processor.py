12469b2c05bc92f1beeeb549827a01140c8a6309
unknown <bionicbiplov45@gmail.com>
2019-06-27 10:09:49 +0700

    fix google maps api issue

--- postfirerecovery/context_processor.py
+++ postfirerecovery/context_processor.py
@@ -4,5 +4,6 @@ from django.conf import settings
 
 def variable_settings(request):
     return {
-        'GOOGLE_ANALYTICS_ID': 'UA-29286089-2',
+        'GOOGLE_ANALYTICS_ID': settings.GOOGLE_ANALYTICS_ID,
+        'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY,
     }
