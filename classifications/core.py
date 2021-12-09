e6b9c0fd8ab4075cb530e505fae91d6c9064c7da
John Dilger <johnjdilger@gmail.com>
2021-12-09 03:12:37 -0600

    adding Lassen to AOI

--- classifications/core.py
+++ classifications/core.py
@@ -14,10 +14,11 @@ class Classification:
         Google Earth Engine API
     """
 
-    ee.Initialize(settings.EE_CREDENTIALS)
+    ee.Initialize()#settings.EE_CREDENTIALS)
 
     # AOI = ee.FeatureCollection('users/biplov/postfirerecovery/AOI/sierra_plus_plumas')
-    AOI = ee.FeatureCollection('users/biplov/postfirerecovery/AOI/aoi')
+    # AOI = ee.FeatureCollection('users/biplov/postfirerecovery/AOI/aoi')
+    AOI = ee.FeatureCollection('users/TEST/CAFire/StudyAreas/AOI_20211207')
     GEOMETRY = AOI.geometry()
 
     # burn parameters
